6a8c4481d3e72d9b80f2f0f1696a05eeb365efe3
neychok <n.kalaidjiev@gmail.com>
2021-10-04 19:07:24 +0300

    init

---
+++ src/setup.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+UNAME=$( command -v uname);
+
+USER="linux";
+FROM_PRE_COMMIT_PATH="./src/linux-pre-commit";
+TO_PRE_COMMIT_PATH="../.git/hooks/pre-commit";
+
+case $( "${UNAME}" | tr '[:upper:]' '[:lower:]') in
+  msys*|cygwin*|mingw*)
+    # or possible 'bash on windows'
+    USER="windows"
+	FROM_PRE_COMMIT_PATH=".\\src\\windows-pre-commit";
+	TO_PRE_COMMIT_PATH="..\\.git\\hooks\\pre-commit";
+    ;;
+  nt|win*)
+    USER="windows"
+	FROM_PRE_COMMIT_PATH=".\\src\\windows-pre-commit";
+	TO_PRE_COMMIT_PATH="..\\.git\\hooks\\pre-commit";
+    ;;
+esac
+
+if [ -e "$TO_PRE_COMMIT_PATH" ];
+then
+	PRE_COMMIT_EXISTS=1
+else
+	PRE_COMMIT_EXISTS=0
+fi
+cp "$FROM_PRE_COMMIT_PATH" "$TO_PRE_COMMIT_PATH"
+if [ "$USER" = "linux" ];
+then
+	chmod +x "$TO_PRE_COMMIT_PATH"
+fi
+
+if [ "$PRE_COMMIT_EXISTS" = 0 ];
+then
+    echo "Pre-commit git hook is installed!"
+else
+    echo "Pre-commit git hook is updated!"
+fi
